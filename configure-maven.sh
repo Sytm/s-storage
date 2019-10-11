580eaa938547f21197d89a577a13eb6bbe0cf1c5
Lukas Planz <lukas.planz@web.de>
2019-10-11 16:55:05 +0200

    Remove bitbucket pipelines stuff

--- configure-maven.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i~ "/<servers>/ a\
-<server>\
-  <id>sytm-nexus</id>\
-  <username>${SYTM_NEXUS_USERNAME}</username>\
-  <password>${SYTM_NEXUS_PASSWORD}</password>\
-</server>" /usr/share/maven/conf/settings.xml
